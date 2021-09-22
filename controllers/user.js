ae052444708a984686c45fa09bb017055273881a
Manu <mcufre84@gmail.com>
2021-09-22 12:00:35 +0200

    upload profile photo functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,15 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const User = require("../models/user.js");
-var { jwtSecret } = require("../config/config.js");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.js');
+var { jwtSecret } = require('../config/config.js');
 
 exports.signin = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     let errorMessages = {
-      emailError: "",
-      passwordError: "",
+      emailError: '',
+      passwordError: '',
     };
 
     const existingUser = await User.findOne({ email });
@@ -21,11 +21,11 @@ exports.signin = async (req, res) => {
 
     const isPasswordCorrect = await bcrypt.compare(
       password,
-      existingUser.password
+      existingUser.password,
     );
 
     if (!isPasswordCorrect) {
-      errorMessages.passwordError = "Invalid password";
+      errorMessages.passwordError = 'Invalid password';
       return res.status(400).json(errorMessages);
     }
 
@@ -35,7 +35,7 @@ exports.signin = async (req, res) => {
         id: existingUser._id,
       },
       jwtSecret,
-      { expiresIn: "1h" }
+      { expiresIn: '1h' },
     );
 
     res.status(200).json({ result: existingUser, token });
@@ -52,16 +52,16 @@ exports.signup = async (req, res) => {
     const existingUser = await User.findOne({ name: userName });
 
     let errorMessages = {
-      userNameError: "",
-      emailError: "",
-      passwordError: "",
+      userNameError: '',
+      emailError: '',
+      passwordError: '',
     };
 
     if (existingUser)
-      errorMessages.userNameError = "User name is already taken";
+      errorMessages.userNameError = 'User name is already taken';
 
     if (existingEmail)
-      errorMessages.emailError = "An user with that email already exists";
+      errorMessages.emailError = 'An user with that email already exists';
 
     if (password !== repeatPassword)
       errorMessages.passwordError = "Passwords don't match";
@@ -82,11 +82,20 @@ exports.signup = async (req, res) => {
     });
 
     const token = jwt.sign({ email: result.email, id: result._id }, jwtSecret, {
-      expiresIn: "1h",
+      expiresIn: '1h',
     });
 
     res.status(200).json({ result, token });
   } catch (error) {
     res.status(500).json({ message: error });
+  }
+};
+
+exports.uploadFile = async (req, res) => {
+  const { file, userId } = req.body;
+  try {
+    await User.findByIdAndUpdate(userId, { imageUrl: file });
+  } catch (error) {
+    res.status(404).send();
   }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 
-const { signin, signup } = require("../controllers/user.js");
+const { signin, signup, uploadFile } = require('../controllers/user.js');
+const { auth } = require('../middleware/auth.js');
 
 const router = express.Router();
 
-router.post("/signin", signin);
-router.post("/signup", signup);
+router.post('/signin', signin);
+router.post('/signup', signup);
+router.post('/uploadFile', auth, uploadFile);
 
 module.exports = router;
